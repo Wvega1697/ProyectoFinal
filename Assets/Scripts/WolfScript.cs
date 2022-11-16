dfa1377bd95b85f323f0bd75c7020d5e6cbda38e
WVEGA1697 <williamsv1697@gmail.com>
2022-11-16 05:24:21 -0600

    Cambios de spawn, salto y velocidad

--- Assets/Scripts/WolfScript.cs
+++ Assets/Scripts/WolfScript.cs
@@ -4,8 +4,8 @@ public class WolfScript : MonoBehaviour
 {
     //Private
     Animator animator;
-    bool jump = true;
-    float jumpTimer = 0, jumpRestartTimer = 1f;
+    bool jump = false;
+    float jumpTimer = 0, jumpRestartTimer = 0.7f;
     float dodgeTimer = 0, dodgeRestartTimer = 1f;
     float respawnTimer = 0, respawnRestartTimer = 3f;
     bool dodge = false;
@@ -16,12 +16,15 @@ public class WolfScript : MonoBehaviour
     public Vector3 _position  = new Vector3(-0.1f, 0.61f, -1.5f);
     public AudioSource audioBark, audioDoubleBark, music, audioDie;
     public GameObject ReintentarImage;
+    public TrailRenderer trail;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         pos = 0;
         respawnTimer = respawnRestartTimer;
+        Time.timeScale = 1;
+        trail.emitting = true;
     }
 
     void Update()
@@ -29,7 +32,8 @@ public class WolfScript : MonoBehaviour
         if (animator.GetBool("Live"))
         {
             JumpLogic();
-            if (jump) MoveLogic();
+            //if (!jump)
+            MoveLogic();
         }
         else if (respawnTimer <= 0)
         {
@@ -43,23 +47,9 @@ public class WolfScript : MonoBehaviour
         {
             respawnTimer -= Time.deltaTime;
         }
-        
-    }
-
-    void DodgeLogic()
-    {
-        if (dodgeTimer > 0)
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            if (dodgeTimer < 0.85f)
-            {
-                dodge = false;
-                //animator.SetBool("DodgeLeft", dodge);//animator.SetBool("DodgeRight", dodge);
-            }
-            dodgeTimer -= Time.deltaTime;
-        }
-        else 
-        {
-            if (jump) MoveLogic();
+            Time.timeScale += 0.25f;
         }
     }
 
@@ -113,32 +103,36 @@ public class WolfScript : MonoBehaviour
     {
         if(jumpTimer <= 0)
         {
-            jump = true;
-            _magic.SetActive(jump);
-            if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            jump = false;
+            trail.emitting = true;
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
+                jump = true;
                 jumpTimer = jumpRestartTimer;
-                animator.SetBool("Jump", jump);
                 audioDoubleBark.Play();
+                trail.emitting = false;
             }
+            animator.SetBool("Jump", jump);
+            _magic.SetActive(!jump);
         }
         else 
         {
-            jump = false;
-            if (jumpTimer < 0.73f) animator.SetBool("Jump", jump);
             jumpTimer -= Time.deltaTime;
-            _magic.SetActive(jump);
         }
     }
     
     void OnTriggerEnter(Collider col)
     {
-        
-        if((col.transform.gameObject.CompareTag("DangerousAll")) || (col.transform.gameObject.CompareTag("DangerousDown") && jump) || (col.transform.gameObject.CompareTag("DangerousUp") && !jump))
+        if((col.transform.gameObject.CompareTag("DangerousAll")) || 
+            col.transform.gameObject.CompareTag("DangerousDown") || 
+            col.transform.gameObject.CompareTag("DangerousUp"))
         {
-           animator.SetBool("Live", false);
-           music.Stop();
-           audioDie.Play();
+            animator.SetBool("Live", false);
+            music.Stop();
+            audioDie.Play();
+            jump = false;
+            animator.SetBool("Jump", false);
+            _magic.SetActive(false);
         }
     }
 
